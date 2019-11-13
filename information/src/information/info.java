5a4f99b5e30b47fb6d5be4852c10b13cacbb3c5c
Junhyun Kang <onenol629@Naver.com>
2019-11-14 01:34:14 +0900

    hotfix commit

--- information/src/information/info.java
+++ information/src/information/info.java
@@ -4,8 +4,8 @@ public class info {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("원광대학교");
-		System.out.println("강준현");
+		System.out.println("20131145");
+		System.out.println("컴퓨터소프트웨어학과");
 
 	}
 
