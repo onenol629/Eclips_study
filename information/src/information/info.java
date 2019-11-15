5d5aba1735791da3f8e50ad416e5f9b6141b3abc
onenol629 <55770631+onenol629@users.noreply.github.com>
2019-11-15 15:25:18 +0900

    eclips commit

--- information/src/information/info.java
+++ information/src/information/info.java
@@ -4,12 +4,12 @@ public class info {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Wonkwang univ");
-		System.out.println("Junhyun Kang");
-		System.out.println("20131145");
-		System.out.println("ComputerSoftware");
-		System.out.println("26");
-		System.out.println("Student");
+		System.out.println("Seoul univ");
+		System.out.println("Gildong Hong");
+		System.out.println("20131234");
+		System.out.println("electric");
+		System.out.println("20");
+		System.out.println("student");
 	
 
 	}
