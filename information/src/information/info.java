eede7ed1c657a10f7767fc908278a8a879fbb11f
onenol629 <55770631+onenol629@users.noreply.github.com>
2019-11-15 14:45:16 +0900

    add commit

--- information/src/information/info.java
+++ information/src/information/info.java
@@ -4,14 +4,13 @@ public class info {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("원광대학교");
-		System.out.println("강준현");
+		System.out.println("서울대학교");
+		System.out.println("홍길동");
 		System.out.println("20131145");
 		System.out.println("컴퓨터소프트웨어학과");
 		System.out.println("26살");
 		System.out.println("예비군");
-		//이 아래는 깃 허브에서 수정하였습니다.
-		System.out.println("깃 허브에서 수정한 문구");
+	
 
 	}
 
