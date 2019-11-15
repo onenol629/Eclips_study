95238ad8107c959f53bb56131bd99754f27fbe1f
onenol629 <55770631+onenol629@users.noreply.github.com>
2019-11-15 15:08:35 +0900

    github commit

--- information/src/information/info.java
+++ information/src/information/info.java
@@ -4,11 +4,11 @@ public class info {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Wonkwang univ");
-		System.out.println("JunHyun Kang");
-		System.out.println("20131145");
-		System.out.println("ComputerSoftware");
-		System.out.println("20");
+		System.out.println("Seoul univ");
+		System.out.println("Gildong Hong");
+		System.out.println("20191234");
+		System.out.println("Electric");
+		System.out.println("26");
 		System.out.println("Student");
 	
 
