77849763c8f5dcb630d71d8642393f86a2756fce
Junhyun Kang <onenol629@Naver.com>
2019-11-15 15:33:11 +0900

    eclips commit

--- information/src/information/info.java
+++ information/src/information/info.java
@@ -4,12 +4,12 @@ public class info {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Wonkwang univ");
-		System.out.println("Junhyun Kang");
-		System.out.println("20131145");
-		System.out.println("ComputerSoftware");
-		System.out.println("26");
-		System.out.println("Students");
+		System.out.println("Wonkwang univ.");
+		System.out.println("junhyun Kang.");
+		System.out.println("20131145.");
+		System.out.println("computerSoftware.");
+		System.out.println("26.");
+		System.out.println("Students.");
 	
 
 	}
